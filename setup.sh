e4d952e0ceeb6c40ba62e2a0f4eab39330399732
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 19:32:45 -0600

    pls work

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Loading......."
 cp -r .config/* ~/.config/ 
 cp -r .bashrc ~/
 cp -r .tmux.conf ~/
-cp  -r.local/share/nvim ~/.local/share/ 
+cp -r .local/share/nvim ~/.local/share/ 
 echo "Within the extra directory located in ~/Documents/Nixos-build/temp"
 echo "is my NixOS configuration file, I would recommend adding whatever you"
 echo "want you yours."
