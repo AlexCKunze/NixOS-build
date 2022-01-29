54b0c323c5fdf805c2c061b05253cd3be78f54b2
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 19:11:33 -0600

    NEXT

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Loading......."
-mkdir -p ~/Documents/NixOS-build/temp
-cp -r .NixOS-build/temp
-cd .NixOS-build/temp
 mv -r .config/* ~/.config
 mv .bashrc ~/
 mv .tmux.conf ~/
-mv .local/share/nvim ~/.local/share/nvim
+mv .local/share/nvim ~/.local/share/ || mkdir ~/.local/share/ ; mv .local/share/nvim ~/.local/share/
 echo "Done...."
 echo "Within the extra directory located in ~/Documents/Nixos-build/temp"
 echo "is my NixOS configuration file, I would recommend adding whatever you"
