130ddea6d4e2d763b6cd348f0127747f59b5b6ff
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 20:27:56 -0600

    Finally

--- setup.sh
+++ setup.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
 
+echo -e "*********************************************************************\n"
+
 
 echo -e "Assimilating to my liking....."
 
 cp -r .config/* ~/.config/ > /dev/null 
-echo -e ".......................\n"
 cp -r .bashrc ~/ > /dev/null
-echo -e "****...................\n"
 cp -r .tmux.conf ~/ > /dev/null
-echo -e "**********.............\n"
 cp -r .local/share/nvim ~/.local/share/ > /dev/null
-echo -e "**************.........\n"
 cp -r .local/share/fonts/ ~/.local/share/ > /dev/null
-echo -e "*******************....\n"
 cp -r .alacritty.yml ~/ > /dev/null
-echo -e "********************...\n"
+
+
+cat gigachad.txt
+
 mkdir -p ~/Pictures/wallpaper && cd ~/Pictures/wallpaper/ > /dev/null
-wget https://cdn.discordapp.com/attachments/658513471813713950/936805613630017546/knight.jpg > /dev/null
+wget -q https://cdn.discordapp.com/attachments/658513471813713950/936805613630017546/knight.jpg
 
 fc-cache -fv > /dev/null
-echo -e "***********************\n"
 echo -e " "
 echo -e "ASSIMILATION HAS COMPLETED"
 
-cat gigachad.txt
 
 source ~/.bashrc
 
@@ -34,3 +32,4 @@ echo -e "want to you yours."
 echo -e "To install nerdtree on neovim just do :PlugInstall > Enter"
 
 
+echo -e "*********************************************************************"
