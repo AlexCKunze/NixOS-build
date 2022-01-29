172cc5a80d56b8949fbf3f27b905782d24e8f0d1
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 19:31:30 -0600

    pls work

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Loading......."
-mv .config/* ~/.config || echo "Please Delete your current i3 folder ~/.config/i3"
-mv .bashrc ~/ || echo "You already have a .bashrc, so take what you like from it and shove it in your own bashrc"
-mv .tmux.conf ~/ || echo "You already have a ~/.tmux.conf, so shove what you like in the one in here into your own"
-mv .local/share/nvim ~/.local/share/ || mkdir ~/.local/share/ ; mv .local/share/nvim ~/.local/share/ || echo "Looks like you already have an nvim config ~/.local/share/nvim"
+cp -r .config/* ~/.config/ 
+cp -r .bashrc ~/
+cp -r .tmux.conf ~/
+cp  -r.local/share/nvim ~/.local/share/ 
 echo "Within the extra directory located in ~/Documents/Nixos-build/temp"
 echo "is my NixOS configuration file, I would recommend adding whatever you"
 echo "want you yours."
