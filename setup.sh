ea31a8beb2dddf17bc793b3d00ec6df24acefff7
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 19:58:23 -0600

    murder

--- setup.sh
+++ setup.sh
@@ -3,17 +3,19 @@
 
 
 echo "Assimilating to my liking....."
-cp -r .config/* ~/.config/ 
-cp -r .bashrc ~/
-cp -r .tmux.conf ~/
-cp -r .local/share/nvim ~/.local/share/
-cp -r .local/share/fonts/ ~/.local/share/
-cp -r .alacritty.yml ~/
 
-source ~/.bashrc
-fc-cache -fv
+cp -r .config/* ~/.config/ > /dev/null 
+cp -r .bashrc ~/ > /dev/null
+cp -r .tmux.conf ~/ > /dev/null
+cp -r .local/share/nvim ~/.local/share/ > /dev/null
+cp -r .local/share/fonts/ ~/.local/share/ > /dev/null
+cp -r .alacritty.yml ~/ > /dev/null
+
+fc-cache -fv > /dev/null
 
 cat gigachad.txt
+
+source ~/.bashrc
 
 echo "Within the extra directory located in ~/Documents/Nixos-build/temp"
 echo "is my NixOS configuration file, I would recommend adding whatever you"
