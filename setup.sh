e1a640fc19360b5b9d0a9ce885c342b77c057a41
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 19:38:22 -0600

    Pls Work

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ echo "Loading......."
 cp -r .config/* ~/.config/ 
 cp -r .bashrc ~/
 cp -r .tmux.conf ~/
-cp -r .local/share/nvim ~/.local/share/ 
+cp -r .local/share/nvim ~/.local/share/
+cp -r .local/share/fonts/ ~/.local/share/
+source ~/.bashrc
+fc-cache -fv
 echo "Within the extra directory located in ~/Documents/Nixos-build/temp"
 echo "is my NixOS configuration file, I would recommend adding whatever you"
 echo "want you yours."
+echo "To install nerdtree on neovim just do :PlugInstall > Enter"
