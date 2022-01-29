1640d0b605858802c609ac7c68e3591bc2006903
sarcasticdream <kayakalotmlg@gmail.com>
2022-01-28 20:17:37 -0600

    Assimilation

--- setup.sh
+++ setup.sh
@@ -2,24 +2,35 @@
 
 
 
-echo "Assimilating to my liking....."
+echo -e "Assimilating to my liking....."
 
 cp -r .config/* ~/.config/ > /dev/null 
+echo -e ".......................\n"
 cp -r .bashrc ~/ > /dev/null
+echo -e "****...................\n"
 cp -r .tmux.conf ~/ > /dev/null
+echo -e "**********.............\n"
 cp -r .local/share/nvim ~/.local/share/ > /dev/null
+echo -e "**************.........\n"
 cp -r .local/share/fonts/ ~/.local/share/ > /dev/null
+echo -e "*******************....\n"
 cp -r .alacritty.yml ~/ > /dev/null
+echo -e "********************...\n"
+mkdir -p ~/Pictures/wallpaper && cd ~/Pictures/wallpaper/ > /dev/null
+wget https://cdn.discordapp.com/attachments/658513471813713950/936805613630017546/knight.jpg > /dev/null
 
 fc-cache -fv > /dev/null
+echo -e "***********************\n"
+echo -e " "
+echo -e "ASSIMILATION HAS COMPLETED"
 
 cat gigachad.txt
 
 source ~/.bashrc
 
-echo "Within the extra directory located in ~/Documents/Nixos-build/extra"
-echo "is my NixOS configuration file, I would recommend adding whatever you"
-echo "want to you yours."
-echo "To install nerdtree on neovim just do :PlugInstall > Enter"
+echo -e "Within the extra directory located in ~/Documents/Nixos-build/extra"
+echo -e "is my NixOS configuration file, I would recommend adding whatever you"
+echo -e "want to you yours."
+echo -e "To install nerdtree on neovim just do :PlugInstall > Enter"
 
 
